be87545e1733ffab178662ef5b7c587afb6cf19b
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-02 23:56:00 -0300

    Alterado ex003

--- ex003.py
+++ ex003.py
@@ -2,4 +2,4 @@
 num1 = int(input("\nDigite um valor: "))
 num2 = int(input("\nDigite outro valor: "))
 print(f"\nA soma entre {num1} e {num2} é igual a {num1 + num2} !\n")
-# Este script pede dois números inteiros e imprime a soma deles.
+# Este script pede dois valores inteiros e imprime a soma deles.
