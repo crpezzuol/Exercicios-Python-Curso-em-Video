8076c934143ed873886adda3997a09bd4b10791f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-03 14:20:08 -0300

    Create ex007
    Média Aritmética

---
+++ ex007.py
@@ -0,0 +1,3 @@
+nota1 = float(input("\nDigite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+print(f"\nA média entre {nota1:.1f} e {nota2:.1f} é igual a {(nota1 + nota2) / 2:.2f}\n")
