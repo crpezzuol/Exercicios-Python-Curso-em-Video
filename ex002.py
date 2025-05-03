2bea69d92682bb27fa0fb7160dda4457812a8c31
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-02 23:54:39 -0300

    Alterado ex002

--- ex002.py
+++ ex002.py
@@ -1,4 +1,4 @@
 # Escreva um programa que peça o nome do usuário e mostre uma mensagem de boas-vindas.
 # Este script pede o nome do usuário e imprime uma mensagem de boas-vindas.
-nome = input("Qual o seu nome? ")
+nome = input("\nQual o seu nome? ")
 print(f"\nOlá {nome}, é um prazer te conhecer!\n")
