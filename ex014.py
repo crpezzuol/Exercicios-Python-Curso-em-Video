e0a554c1179c56aff7f0567353fbd4f06f135cd2
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-03 23:23:47 -0300

    Create ex014
    Conversor de Temperaturas

---
+++ ex014.py
@@ -0,0 +1,4 @@
+temp = float(input("\nInforme a temperatura em °C: "))
+# Converte Celsius em Fahrenheit 
+temp_f = (temp * 9/5) + 32
+print(f"A temperatura de {temp}°C corresponde a {temp_f:.1f}°F!\n")
