69aec91332fd82240d5d6fb1831a9c67e45e8c2f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-04 21:49:17 -0300

    Create ex017

---
+++ ex017.py
@@ -0,0 +1,4 @@
+oposto = float(input('\nDigite o comprimento do cateto oposto: '))
+adjacente = float(input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = (oposto**2 + adjacente**2)**0.5
+print(f'\nA hipotenusa vai medir: {hipotenusa:.2f}\n')
