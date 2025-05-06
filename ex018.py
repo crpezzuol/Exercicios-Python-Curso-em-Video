4ab11b1394bb82fe7edca95358ce6e44a1619607
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-05 21:06:02 -0300

    Create ex018

---
+++ ex018.py
@@ -0,0 +1,8 @@
+ang = float(input("\nDigite o ângulo que você deseja: "))
+import math
+seno = math.sin(math.radians(ang))  # Seno
+cosseno = math.cos(math.radians(ang))  # Cosseno        
+tangente = math.tan(math.radians(ang))  # Tangente
+print(f"\nO ângulo de {ang}° tem o SENO de {seno:.2f}")
+print(f"O ângulo de {ang}° tem o COSSENO de {cosseno:.2f}") 
+print(f"O ângulo de {ang}° tem a TANGENTE de {tangente:.2f}\n")
