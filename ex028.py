a52ec9204a788ef52e4d172ae38258b87fcc628d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-05 19:33:24 -0300

    Update ex028

--- ex028.py
+++ ex028.py
@@ -1,5 +1,8 @@
 from random import randint
 computador = randint(0, 5) # sorteia um número entre 0 e 5
-jogador = int(input('Tente adivinhar o número que estou pensando entre 0 e 5: '))
+print('-=-' * 20)
+print('| Vou pensar em um número entre 0 e 5.  Tente adivinhar... |')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? (0 a 5): '))
 if jogador < 0 or jogador > 5:
     print('Número inválido! Tente novamente.')
