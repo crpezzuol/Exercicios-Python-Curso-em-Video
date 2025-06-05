2e18ac9e1cc9d78f23d0612ca7aef5b23cb817b5
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-05 19:49:22 -0300

    Update ex028

--- ex028.py
+++ ex028.py
@@ -4,5 +4,8 @@ print('-=-' * 20)
 print('| Vou pensar em um número entre 0 e 5.  Tente adivinhar... |')
 print('-=-' * 20)
 jogador = int(input('Em que número eu pensei? (0 a 5): ')) # jopgador tenta adivinhar o número
-if jogador < 0 or jogador > 5:
-    print('Número inválido! Tente novamente.')
+if jogador == computador:
+    print('Parabéns! Você conseguiu me vencer!')
+else:
+    print(f'Ganhei! Eu pensei no número {computador} e não no {jogador}.')
+
