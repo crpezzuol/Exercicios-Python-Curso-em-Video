223d795b5ab2e4886d527285b14dac2526d34ecf
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-05 18:35:26 -0300

    Crteate ex027

---
+++ ex027.py
@@ -0,0 +1,4 @@
+nome = str(input("\nDigite seu nome completo: "))
+print('\nÉ um prazer te conhecer!\n')
+print(f'Seu primeiro nome é {nome.split()[0]}.')
+print(f'Seu último nome é {nome.split()[-1]}.\n')
