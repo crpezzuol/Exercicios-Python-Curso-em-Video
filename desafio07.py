c257bc11e323ae5484cee76a5c8687b6040ec1a7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-06 20:59:29 -0300

    Create desafio07
    Peça para o usuário digitar dois números e informe qual é o maior, ou se são iguais.

---
+++ desafio07.py
@@ -0,0 +1,9 @@
+# Peça para o usuário digitar dois números e informe qual é o maior, ou se são iguais.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+if num1 > num2:
+    print(f"O maior número é: {num1}")
+elif num2 > num1:
+    print(f"O maior número é: {num2}")
+else:
+    print("Os números são iguais.")
