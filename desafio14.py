3093672f99a266aca9bbf203d38642fd7257eabb
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-06 23:53:21 -0300

    Update desafio14
    Estudar

--- desafio14.py
+++ desafio14.py
@@ -1,2 +1,10 @@
 # Peça para o usuário digitar uma frase e informe se ela é um palíndromo (lê-se igual de trás para frente, desconsiderando espaços)
 frase = input("Digite uma frase: ").replace(" ", "").lower()
+
+'''
+frase = input('Digite uma frase: ').replace(' ', '').lower()
+if frase == frase[::-1]:
+    print('A frase é um palíndromo!')
+else:
+    print('A frase não é um palíndromo.')
+'''
