ea615ca5e6d720778f6da5053940e7a5b9ee97bb
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-07 14:16:38 -0300

    Create ex029

--- ex028.py
+++ ex028.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep 
 computador = randint(0, 5) # computador sorteia um número entre 0 e 5
 print('-=-' * 20)
-print('| Vou pensar em um número entre 0 e 5.  Tente adivinhar... |')
+print('| Vou pensar em um número entre 0 e 5... Tente adivinhar... |')
 print('-=-' * 20,'\n')
 jogador = int(input('Em que número eu pensei? (0 a 5): ')) # jopgador tenta adivinhar o número
 print('\nPROCESSANDO...')
