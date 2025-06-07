f7ab25d650e4f9b17f0f3ff6d16812cd06cb51a7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-06 23:50:42 -0300

    Create desafio14
    Peça para o usuário digitar uma frase e informe se ela é um palíndromo (lê-se igual de trás para frente, desconsiderando espaços)

---
+++ desafio14.py
@@ -0,0 +1,2 @@
+# Peça para o usuário digitar uma frase e informe se ela é um palíndromo (lê-se igual de trás para frente, desconsiderando espaços)
+frase = input("Digite uma frase: ").replace(" ", "").lower()
