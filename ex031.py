f1ffaa62fc5e8f3aa5a235b51fc609e5df2773ba
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-07 21:26:46 -0300

    Update  ex031

--- ex031.py
+++ ex031.py
@@ -1,6 +1,6 @@
 distancia = float(input("Qual é a distância da sua viagem? "))
 print(f'Você está prestes a começar uma viagem de {distancia} km.')
 if distancia <= 200:
-    print('E o preço da sua passagem será de R$ 75.00')
+    print(f'E o preço da sua passagem será de R$ {distancia * 0.50:.2f}')
 else:
-    print('E o preço de sua passagem será de R$ 94.50')
+    print(f'E o preço de sua passagem será de R$ {distancia * 0.45:.2f}')
