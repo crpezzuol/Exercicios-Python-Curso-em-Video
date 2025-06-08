14d741773ecc4eb19d089f42836c417a69c39d4b
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-07 22:14:04 -0300

    Create aula11
    Using colors in the terminal with the ANSI table

---
+++ aula11.py
@@ -0,0 +1,14 @@
+print('Olá, mundo!')
+print('\33[31mOlá, mundo!')
+print('\033[31;43;1mOlá, mundo!\033[m')
+print('\033[1;31;43mOlá, mundo!\033[m')
+print('\033[1;31;43mOlá, mundo!\033[m')
+print('\033[4;31;44mOlá, mundo!\033[m')
+print('\033[4;30;46mOlá, mundo!\033[m')
+print('\033[7;30mOlá, mundo!\033[m')
+print('\033[0;34;43mOlá, mundo!\033[m') 
+print('\033[7;34;43mOlá, mundo!\033[m')
+a = 3
+b = 5
+print(f'Os valores são \033[32m{a} e \033[31m{b}!!!')
+print(f'\033[mOs valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
