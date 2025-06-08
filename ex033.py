4c3d9cd9e8a2289f1e5699ebdc44b30b78801f91
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-07 23:30:05 -0300

    Update ex033
    Ask the user to input three numbers and show which is the smallest and the largest of them, video course solution.

--- ex033.py
+++ ex033.py
@@ -1,7 +1,21 @@
 a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 c = int(input("Enter a third number: "))
+'''
 maior = max(a, b, c)
 menor = min(a, b, c)
-print(f"The smallest number is: {menor}")
+'''
+
+menor = a
+if b < a and b < c:
+    menor = b
+elif c < a and c < b:
+    menor = c
+maior = a
+if b > a and b > c:
+    maior = b
+elif c > a and c > b:
+    maior = c
+
+print(f"\nThe smallest number is: {menor}")
 print(f"The largest number is: {maior}")
