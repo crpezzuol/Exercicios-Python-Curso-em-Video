267b3e9adca8c193f7e2192d7be8bcd7101b3a4e
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-08 23:54:32 -0300

    Update desafio15
    Ask the user to input three sides and inform if they can form a triangle, and if they can, say whether the triangle is equilateral, isosceles, or scalene

--- desafio15.py
+++ desafio15.py
@@ -3,3 +3,7 @@
 print ('-=' * 18)
 print (' ' * 10 + 'Triangle Analyzer')
 print ('-=' * 18)
+print('\nInput the lengths of the three sides of a triangle:\n')
+a = float(input('First side: '))
+b = float(input('Second side: '))
+c = float(input('Third side: ')) 
