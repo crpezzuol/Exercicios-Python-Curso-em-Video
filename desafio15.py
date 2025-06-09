ac0118c943bfdc437d1701e5d8bb92a80adb557a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-08 23:35:28 -0300

    Create desafio15
    Ask the user to input three sides and inform if they can form a triangle, and if they can, say whether the triangle is equilateral, isosceles, or scalene

---
+++ desafio15.py
@@ -0,0 +1,2 @@
+# Peça para o usuário digitar três lados e informe se eles podem formar um triângulo, e caso possam, diga se o triângulo é equilátero, isósceles ou escaleno
+# Ask the user to input three sides and inform if they can form a triangle, and if they can, say whether the triangle is equilateral, isosceles, or scalene
