e7781b63520d3eaff68b2fd5219984ec3f214b2e
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-10 21:48:26 -0300

    Update bingo

--- Bingo.py
+++ Bingo.py
@@ -6,7 +6,20 @@ def limpa():
     """
     os.system('cls' if os.name == 'nt' else 'clear')    
 
-limpa()
+# Chama a função limpa para limpar a tela ao iniciar o jogo
+
+def exibe_menu():
+    """
+    Exibe o menu principal do jogo Bingo.
+    """
+    limpa() # Chama a função limpa para limpar a tela ao iniciar o jogo
+    print("-=" * 18, "Bingo Game", "-=" * 18)
+    print(" " * 10 + "Menu do Jogo Bingo")
+    print("Menu do Jogo Bingo")
+    print("1. Iniciar Jogo")
+    print("2. Regras do Jogo")
+    print("3. Sair")
+
 def main():
     """
     Função principal do jogo Bingo.
@@ -15,5 +28,5 @@ def main():
     # Aqui você pode adicionar a lógica do jogo de Bingo
     # Por exemplo, gerar números aleatórios, verificar vencedores, etc.
     print("O jogo ainda não está implementado.")
-if __name__ == "__main__":
-    main()
+
+exibe_menu()
