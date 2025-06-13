eb5433b69b3f312e4a6dc17953f11c0aed8502ed
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-13 14:09:46 -0300

    Update aula12
    nested structures

--- aula12.py
+++ aula12.py
@@ -1,1 +1,9 @@
-nome = str(input('Qaul é o seu nome?: '))
+nome = str(input('Qual é o seu nome?: '))
+if nome == 'Meire' or nome == 'Carlos' or nome =='Natan':
+    print('Que nome bonito você tem!')
+elif nome == 'Gustavo':
+    print('Seu nome é bem FEIO!')
+elif nome == 'João' or nome == 'Pedro' or nome == 'Maria' or nome == 'Aparecida':
+    print('Seu nome é bem popular no Brasil!')
+else:
+    print('Seu nome é bem normal!')
