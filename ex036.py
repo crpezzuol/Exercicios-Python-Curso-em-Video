b5631bb959d2ccfee0bb9322a4b7fa95a446840c
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-13 21:17:19 -0300

    Update ex036.py

--- ex036.py
+++ ex036.py
@@ -1,12 +1,12 @@
 print('\n') # Início do exercício
 # Exercício 036: Gerenciador de Pagamentos
 print('=-' * 20 + ' Gerenciador de Pagamentos ' + '-=' * 20)
-casa = int(input('Qual o valor da casa?: R$ '))
-salario = int(input('Qual o salário do comprador?: R$ '))
-anos = int(input('Em quantos anos pretende financiar?: '))
+casa = int(input('Qual o valor da casa?: R$ \033[32m\033[m'))
+salario = int(input('\033[mQual o salário do comprador?: R$ \033[32m'))
+anos = int(input('\033[mEm quantos anos pretende financiar?: \033[32m'))
 prestacao = casa / (anos * 12)
 print('-' * 100)
-print(f'Para pagar uma casa de R$ {casa:.2f} em {anos} anos, a prestação mensal será de R$ {prestacao:.2f}')
+print(f'Para pagar uma casa de R$ \033[32m{casa:.2f}\033[m em \033[32m{anos}\033[m anos, a prestação mensal será de R$ {prestacao:.2f}')
 if prestacao > (salario * 0.3):
     print(F'Empréstimo negado! A prestação de R$ {prestacao:.2f} ultrapassa 30% do seu salário.')
 else:
