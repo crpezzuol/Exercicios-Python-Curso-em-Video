627ea8e6d69628091ed5f5a4fe726e3e24d1153a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-13 21:39:50 -0300

    Update eex036
    nested structures

--- ex036.py
+++ ex036.py
@@ -1,16 +1,17 @@
 print('\n') # Início do exercício
 # Exercício 036: Gerenciador de Pagamentos
-print('=-' * 20 + ' Gerenciador de Pagamentos ' + '-=' * 20)
-casa = int(input('Qual o valor da casa?: R$ \033[32m\033[m'))
+print('\033[33m=-' * 19 + ' Gerenciador de Pagamentos ' + '-=' * 19 + '\033[m')
+casa = int(input('Qual o valor da casa?: R$ \033[32m'))
 salario = int(input('\033[mQual o salário do comprador?: R$ \033[32m'))
 anos = int(input('\033[mEm quantos anos pretende financiar?: \033[32m'))
+print('\033[m')
 prestacao = casa / (anos * 12)
-print('-' * 100)
+print('\033[33m-\033[m' * 103)
 print(f'Para pagar uma casa de R$ \033[32m{casa:.2f}\033[m em \033[32m{anos}\033[m anos, a prestação mensal será de R$ {prestacao:.2f}')
 if prestacao > (salario * 0.3):
-    print(F'Empréstimo negado! A prestação de R$ {prestacao:.2f} ultrapassa 30% do seu salário.')
+    print(F'Empréstimo \033[31mNEGADO!\033[m A prestação de R$ {prestacao:.2f} ultrapassa 30% do seu salário.')
 else:
-    print(F'Empréstimo aprovado! A prestação de R$ {prestacao:.2f} está dentro do limite de 30% do seu salário.')
-print('-' * 100)
+    print(F'Empréstimo \033[32mAPROVADO!\033[m A prestação de R$ {prestacao:.2f} está dentro do limite de 30% do seu salário.')
+print('\033[33m-' * 103 + '\033[m')
 print('\n')
 # Fim do exercício
