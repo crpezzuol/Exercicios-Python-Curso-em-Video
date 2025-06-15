fc4307e9dcdce56975549f78b4f7d07a06ad5031
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-15 19:48:22 -0300

    Update ex038

--- ex038.py
+++ ex038.py
@@ -1,5 +1,6 @@
 # This program compares two numbers input by the user.
 
+
 # This program compares two numbers input by the user.
 def clear_screen():
     import os
@@ -8,7 +9,7 @@ def clear_screen():
 # This function prints a header message to introduce the program.
 def print_header():
     print('\033[1;33m-=' * 36)
-    print('\033[1;33m-' * 23,'Number Comparison Program', 22 * '-')
+    print('\033[1;33m-' * 23,'\033[1;34mNumber Comparison Program\033[m', 22 * '-')
     print('\033[1;33m-=' * 36)
     print('\033[m')
 
@@ -20,6 +21,7 @@ def print_instructions():
 
 # Prompt the user to enter two numbers
 def prompt_user(): 
+    global n1, n2
     n1 = float(input("Enter first number: \033[32m"))
     n2 = float(input("\033[mEnter second number: \033[32m"))
     print('\033[m')
@@ -37,17 +39,18 @@ def compare_numbers(n1, n2):
 # This function prints a footer message to thank the user for using the program.
 def print_footer():
     print('\033[1;33m-=' * 36)
-    print('\033[1;33mThank you for using the number comparison program!\033[m')
+    print('\033[1;34mThank you for using the number comparison program!\033[m')
     print('\033[1;33m-=' * 36)
     print('\033[m')
+    print('\n')
 
 # Main program execution
 def main(): 
     clear_screen()
     print_header()
     print_instructions()
-    #prompt_user()
-    #compare_numbers(n1, n2)
+    prompt_user()
+    compare_numbers(n1, n2)
     print_footer()
 
 main()
