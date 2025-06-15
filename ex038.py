42b0a48611acf0beed4f6474f915d4cfa32f24ba
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-15 19:08:59 -0300

    Update ex038

--- ex038.py
+++ ex038.py
@@ -1,9 +1,25 @@
 # This program compares two numbers input by the user.
 
-print('\033[1;33m-=' * 20)
-print('\033[1;33m''-' * 10, 'Number  Comparison', '-' * 10, '\033[m')    
-print('\033[1;33m-=' * 20)
-print('\033[m')
+def clear_screen():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_header():
+
+    print('\033[1;33m-=' * 20)
+    print('\033[1;33m---- Number Comparison Program ----\033[m')
+    print('\033[1;33m-=' * 20)
+    print('\033[m')
+
+def print_instructions():
+    print('\033[1;33mPlease enter two numbers to compare them.\033[m')
+    print('\033[1;33mThe program will tell you which number is greater, or if they are equal.\033[m')
+    print('\033[m')
+
+def print_footer():
+    print('\033[1;33mThank you for using the number comparison program!\033[m')
+    print('\033[1;33m-=' * 20)
+    print('\033[m')
 
 n1 = float(input("Enter first number: \033[32m"))
 n2 = float(input("\033[mEnter second number: \033[32m"))
@@ -20,3 +36,8 @@ else:
 print('\n')
 print("Comparison complete.")
 print('\n')
+
+clear_screen()
+print_header()  
+print_instructions()
+print_footer()
