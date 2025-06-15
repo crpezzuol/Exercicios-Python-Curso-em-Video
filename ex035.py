105a84357c6afab322f80dbfa0e54c1f6aac479f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-14 22:49:09 -0300

    Update ex035
    insert comments

--- ex035.py
+++ ex035.py
@@ -1,3 +1,6 @@
+# Triangle Analyzer
+# This program checks if three given lengths can form a triangle.
+
 print ('-=' * 18)
 print (' ' * 10 + 'Triangle Analyzer')
 print ('-=' * 18)
@@ -5,6 +8,8 @@ print('\nInput the lengths of the three sides of a triangle:\n')
 a = float(input('First side: '))
 b = float(input('Second side: '))
 c = float(input('Third side: ')) 
+
+# Check if the lengths can form a triangle
 
 if a + b > c and a + c > b and b + c > a:
     print('\nThe lengths can form a triangle.\n')
