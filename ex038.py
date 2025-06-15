471346bf7ab445fde911937eec36b548d73bc53f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-15 18:45:03 -0300

    Update ex038

--- ex038.py
+++ ex038.py
@@ -1,3 +1,9 @@
+# This program compares two numbers input by the user.
+
+print('\033[1;33m-=' * 20)
+print('\033[1;33m''-' * 10, 'Number  Comparison', '-' * 10, '\033[m')    
+print('\033[1;33m-=' * 20)
+print('\033[m')
 n1 = float(input("Enter first number: "))
 n2 = float(input("Enter second number: "))
 if n1 > n2:
@@ -5,5 +11,5 @@ if n1 > n2:
 elif n1 < n2:
     print(f"The second number {n2} is greater than the first number {n1}")
 else:
-    print(f"The two numbers are equal: {n1} = {n2}.")
+    print(f"The two numbers are equal: {n1} = {n2}")
 print("Comparison complete.")
