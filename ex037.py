bcd4a22ab179440a38160b255ef8295d1049bd67
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-15 15:39:14 -0300

    Update ex037

--- ex037.py
+++ ex037.py
@@ -1,4 +1,11 @@
 
 # This script prompts the user to enter an integer and then prints it in different number bases.
 
-num = int(input("Please enter an integer number: "))
+num = int(input("Please enter an integer number: \033[32m"))
+print('\033[m')
+base = int(input('Choose one of the numeric bases for conversion: \033[32m'
+              '\n1 - Binary'    
+              '\n2 - Octal'
+              '\n3 - Hexadecimal'
+              '\n\033[mYour choice: '))
+              
