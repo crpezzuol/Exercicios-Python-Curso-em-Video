1306d43f78c56f698b1e761c28bf75d9844fdc1c
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-15 18:38:17 -0300

    Update ex038

--- ex038.py
+++ ex038.py
@@ -1,3 +1,9 @@
 n1 = float(input("Enter first number: "))
 n2 = float(input("Enter second number: "))
-if
+if n1 > n2:
+    print(f"The first number {n1} is greater than the second number {n2}")
+elif n1 < n2:
+    print(f"The second number {n2} is greater than the first number {n1}")
+else:
+    print(f"The two numbers are equal: {n1} = {n2}.")
+print("Comparison complete.")
