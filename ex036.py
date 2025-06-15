94ae842841b2c48537e03ffd19a1f21102d4fd9f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-14 22:44:09 -0300

    Update ex036
    insert comments

--- ex036.py
+++ ex036.py
@@ -9,14 +9,18 @@ anos = int(input('\033[mEm quantos anos pretende financiar?: \033[32m'))
 print('\033[m')
 
 # Cálculo da prestação mensal
+
 prestacao = casa / (anos * 12)
 print('\033[33m-\033[m' * 103)
 print(f'Para pagar uma casa de R$ \033[32m{casa:.2f}\033[m em \033[32m{anos}\033[m anos, a prestação mensal será de R$ {prestacao:.2f}')
+
 # Verificação se a prestação mensal ultrapassa 30% do salário
+
 if prestacao > (salario * 0.3):
     print(F'Empréstimo \033[31mNEGADO!\033[m A prestação de R$ {prestacao:.2f} ultrapassa 30% do seu salário.')
 else:
     print(F'Empréstimo \033[32mAPROVADO!\033[m A prestação de R$ {prestacao:.2f} está dentro do limite de 30% do seu salário.')
+    
 print('\033[33m-' * 103 + '\033[m')
 print('\n')
 
