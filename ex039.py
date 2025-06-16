1a547961d87421617599ebd8b3f17f2af6fde81a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-16 19:41:40 -0300

    Update ex039

--- ex039.py
+++ ex039.py
@@ -1,7 +1,8 @@
 
 from datetime import date
 
-ano = int(input("Digite seu ano de nascimento: "))
-ano = date.today().year
-
-print(ano)
+ano_nasc = int(input("Digite seu ano de nascimento: "))
+ano_atual = date.today().year
+idade = ano_atual - ano_nasc
+if idade < 18:
+    print(f"Você ainda não atingiu a maioridade. Faltam {18 - idade} anos para você completar 18 anos.")
