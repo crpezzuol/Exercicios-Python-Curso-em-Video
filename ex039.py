04ac9236512d54c39d1f7450be8b18d0494f71b5
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-16 19:54:37 -0300

    Update ex039.py

--- ex039.py
+++ ex039.py
@@ -5,4 +5,4 @@ ano_nasc = int(input("Digite seu ano de nascimento: "))
 ano_atual = date.today().year
 idade = ano_atual - ano_nasc
 if idade < 18:
-    print(f"Você ainda não atingiu a maioridade. Faltam {18 - idade} anos para você completar 18 anos.")
+    print(f"Quem nasceu em {ano_nasc} tem {idade} anos em {ano_atual}.")
