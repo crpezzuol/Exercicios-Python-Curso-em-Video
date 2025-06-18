6163e81d46f5c551e38e07b2b2781aad3f29e93a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-18 19:31:19 -0300

    Update ex040

--- ex040.py
+++ ex040.py
@@ -2,3 +2,4 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 print(f'Tirando {nota1} e {nota2}, a média do aluno é {media}')
+
