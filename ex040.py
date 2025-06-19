79f96ab2db8e3396caabbb6322bd9e34d2d1a58d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-19 18:14:54 -0300

    Update ex040

--- ex040.py
+++ ex040.py
@@ -4,4 +4,6 @@ media = (nota1 + nota2) / 2
 print(f'Tirando {nota1} e {nota2}, a média do aluno é {media}')
 
 if media < 5:
-    print('O Aluno está \033[31mREPROVADO!\033[m')
+    print('O Aluno está \033[1;31mREPROVADO!\033[m')
+elif media < 6.9:
+    print('O Aluno está de \033[1;33mRECUPERAÇÃO!\033[m')
