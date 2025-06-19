b3c5c4156e763c934ceedc1f44a19d443b0257e6
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-19 18:35:38 -0300

    Update ex040

--- ex040.py
+++ ex040.py
@@ -8,7 +8,7 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 print('\n')
 media = (nota1 + nota2) / 2
-print(f'Tirando {nota1} e {nota2}, a média do aluno é {media}')
+print(f'Tirando {nota1:.1f} e {nota2:.1f}, a média do aluno é {media:.1f}')
 
 if media < 5:
     print('O Aluno está \033[1;31mREPROVADO!\033[m')
