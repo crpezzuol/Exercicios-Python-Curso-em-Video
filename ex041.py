82650a2b57047060abedd96c475407cb653dee53
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-19 21:47:10 -0300

    Update ex041

--- ex041.py
+++ ex041.py
@@ -7,4 +7,10 @@ ano_nasc = int(input('Ano de Nascimento: \033[32m'))
 print('\33[m')
 ano_atual = date.today().year
 idade = ano_atual - ano_nasc
-print(f'O atleta tem {idade}')
+print(f'O atleta tem {idade} anos.')
+if idade < 10:
+    print('Classificação: \033[1;34mMIRIM\033[m')
+elif idade < 15:
+    print('Classificação: \033[1;34mINFANTIL\033[m')
+elif idade < 20:
+    print('Classificação: \033[1;34mMIRIM\033[m')
