0923fc5c939e635e4e7af3062b3165148f834842
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-19 21:50:37 -0300

    Update ex041

--- ex041.py
+++ ex041.py
@@ -14,3 +14,5 @@ elif idade < 15:
     print('Classificação: \033[1;34mINFANTIL\033[m')
 elif idade < 20:
     print('Classificação: \033[1;34mMJUNIOR\033[m')
+elif idade < 26:
+    print('Classificação: \033[1;34mSÊNIOR\033[m')
