3685da6e2e40a4e531855959c25b44d052c14294
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-19 21:38:34 -0300

    Update ex041

--- ex041.py
+++ ex041.py
@@ -3,6 +3,8 @@ import os
 
 
 print('\n')
-nasc = int(input('Ano de Nascimento: \033[32m'))
+ano_nasc = int(input('Ano de Nascimento: \033[32m'))
 print('\33[m')
 ano_atual = date.today().year
+idade = ano_atual - ano_nasc
+print(f'O atleta tem {idade}')
