be4e375a64deff7178c1e0f69828192f6b986825
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-20 23:29:56 -0300

    Update ex042

--- ex042.py
+++ ex042.py
@@ -12,14 +12,14 @@ def cabecalho():
               <><><><><><><><><><><>
            ''')
 
-
-s1 = int(input('Primeiro segmento: \033[32m'))
-s2 = int(input('\033[mSegundo segmento: \033[32m'))
-s3 = int(input('\033[mTerceiro segmento: \033[32m'))
+def seg_triangulos():
+    s1 = int(input('Primeiro segmento: \033[32m'))
+    s2 = int(input('\033[mSegundo segmento: \033[32m'))
+    s3 = int(input('\033[mTerceiro segmento: \033[32m'))
 
 
 def main():
     limpa_tela
     cabecalho
 
-main()
+cabecalho()
