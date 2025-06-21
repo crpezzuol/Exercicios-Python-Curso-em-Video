0d0b2b05725ccb59b796c7313ff5b51ad17355f7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-20 23:27:46 -0300

    Update ex032

--- ex042.py
+++ ex042.py
@@ -3,7 +3,23 @@ import os
 def limpa_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def cabecalho():
+    print('''           <>
+                      <>  <>
+                    <>      <>
+                  <>          <>
+                <>              <>
+              <><><><><><><><><><><>
+           ''')
+
+
 s1 = int(input('Primeiro segmento: \033[32m'))
 s2 = int(input('\033[mSegundo segmento: \033[32m'))
 s3 = int(input('\033[mTerceiro segmento: \033[32m'))
 
+
+def main():
+    limpa_tela
+    cabecalho
+
+main()
