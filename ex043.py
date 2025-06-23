e5c5831c1fe802ec883345def2d9b2e00f684beb
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-22 22:27:49 -0300

    Update ex043

--- ex043.py
+++ ex043.py
@@ -2,5 +2,8 @@ print('\n')
 peso = float(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
 imc = peso / (altura ** 2)
-
-print(imc)
+if imc < 18.5:
+    print(f'Seu IMC está em {imc:.2} e você está ABAIXO DO PESO!')
+elif imc < 25:
+    print(f'Seu IMC está em {imc:.2} e você está de PARABÉNS seu peso está NORMAL!')
+    elif imc < 
