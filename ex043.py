1371e5af020ce9d92feae5a098211793b7bd16c0
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-22 22:59:33 -0300

    Update ex043

--- ex043.py
+++ ex043.py
@@ -4,13 +4,13 @@ altura = float(input('Digite sua altura (mts): '))
 print('\n')
 imc = peso / (altura ** 2)
 if imc < 18.5:
-    print(f'Seu IMC está em {imc:.2} CUIDADO! você está ABAIXO DO PESO!')
+    print(f'Seu IMC está em {imc:.1f} CUIDADO! você está ABAIXO DO PESO!')
 elif imc < 25:
-    print(f'Seu IMC está em {imc:.2} e você está de PARABÉNS seu peso está NORMAL!')
+    print(f'Seu IMC está em {imc:.1f} e você está de PARABÉNS seu peso está NORMAL!')
 elif imc < 30:
-    print(f'Seu IMC está em {imc:.2} e você tem que se CUIDAR porque está com SOBREPESO!')
+    print(f'Seu IMC está em {imc:.1f} e você tem que se CUIDAR porque está com SOBREPESO!')
 elif imc < 40:
-    print(f'Seu IMC está em {imc:.2} CUIDADO! você está OBESO!')
+    print(f'Seu IMC está em {imc:.1f} CUIDADO! você está OBESO!')
 else:
-    print(f'Seu IMC está em {imc:.2} CUIDADO! você está com OBESIDADE MÓRBIDA!')
+    print(f'Seu IMC está em {imc:.1f} CUIDADO! você está com OBESIDADE MÓRBIDA!')
 print('\n')
