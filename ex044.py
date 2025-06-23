c29051185d3aac135ee927a4b2d91ac586175b5e
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-23 00:12:56 -0300

    Update ex044

--- ex044.py
+++ ex044.py
@@ -9,5 +9,11 @@ print('\033[1;32m<>\033[1;33m<>' * 5,'\033[1;34mLOJAS PEZZUOL','\033[1;32m<>\033
 print('\n\033[m')
 compras = float(input('Preço das compras: R$ \033[1;32m'))
 print('\n\033[m')
+print('''FORMAS DE PAGAMENTO>\n
+      [ 1 ] à vista dinheiro/cheque
+      [ 2 ] à vista cartão
+      [ 3 ] 2x no cartão
+      [ 4 ] 3x ou mais no cartão
+''')
 pagamento = int(input('Qual a opção? \033[1;32m'))
 print('\n\033[m')
