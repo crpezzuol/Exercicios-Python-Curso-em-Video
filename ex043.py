156418ba7bc209012b473dc5f778daa65bf5de92
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-22 22:34:36 -0300

    Update ex043

--- ex043.py
+++ ex043.py
@@ -6,4 +6,5 @@ if imc < 18.5:
     print(f'Seu IMC está em {imc:.2} e você está ABAIXO DO PESO!')
 elif imc < 25:
     print(f'Seu IMC está em {imc:.2} e você está de PARABÉNS seu peso está NORMAL!')
-    elif imc < 
+elif imc < 30:
+    print(f'Seu IMC está em {imc:.2} e você tem que se CUIDAR porque está com SOBREPESO!')
