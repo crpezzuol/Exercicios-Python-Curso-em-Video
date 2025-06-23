7df5827f034aa142a567f8fb2f75333849a6f6b1
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-22 23:12:50 -0300

    Update ex043

--- ex043.py
+++ ex043.py
@@ -5,11 +5,11 @@ print('\n')
 imc = peso / (altura ** 2)
 if imc < 18.5:
     print(f'Seu IMC está em {imc:.1f} CUIDADO! você está ABAIXO DO PESO!')
-elif imc < 25:
+elif 18.5 <= imc < 25:
     print(f'Seu IMC está em {imc:.1f} e você está de PARABÉNS seu peso está NORMAL!')
-elif imc < 30:
+elif 25 <= imc < 30:
     print(f'Seu IMC está em {imc:.1f} e você tem que se CUIDAR porque está com SOBREPESO!')
-elif imc < 40:
+elif 30 <= imc < 40:
     print(f'Seu IMC está em {imc:.1f} CUIDADO! você está OBESO!')
 else:
     print(f'Seu IMC está em {imc:.1f} CUIDADO! você está com OBESIDADE MÓRBIDA!')
