5a1c8720204a6016172bac65f09dac6c01fc5501
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-24 16:52:28 -0300

    Update ex045

--- ex045.py
+++ ex045.py
@@ -4,5 +4,21 @@ import random
 def limpa_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def cabecalho():
+    print('=' * 30, '\033[1;34mJOKENPÔ\033[m', '=' * 30)
+    print('\n\033[m')
+    print('''\033[1;34mSuas opções são:\033[m
+    [0] PEDRA
+    [1] PAPEL
+    [2] TESOURA
+    [3] SAI FORA''')
+    print('\n\033[m')
 
-print('=' * 30,'\033[1;34mJOKENPÔ\033[m','=' * 30)
+def main():
+    limpa_tela()
+    cabecalho()
+    
+
+
+
+main()
