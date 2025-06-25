96b492f8e5a4a25d279a044b9479487a48f42cda
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 18:56:41 -0300

    Update ex046

--- ex046.py
+++ ex046.py
@@ -2,6 +2,10 @@ from time import sleep
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
+sleep(1)
 for c in range(10, -1, -1):
     print(c)
     sleep(1)
+print('\033[1;33mBUUMM!!! \033[1;32mPOW!!! \033[1;31mBANG!!!')
+print('\033[m')
+print('\n' * 3)
