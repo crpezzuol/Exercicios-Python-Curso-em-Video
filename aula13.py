a680e863734c7638cf34764e5d0fafca7c9d8272
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 15:26:07 -0300

    Update aula13

--- aula13.py
+++ aula13.py
@@ -1,15 +1,20 @@
 # imprimindo uma mensagem 6 vezes
-
+print('\n')
+print('imprimindo uma mensagem 6 vezes')
 for c in range (0, 6):
     print('Oi!', c)
 print('Fim')
 
 # imprimindo uma mensagem 5 vezes
+print('\n')
+print('imprimindo uma mensagem 5 vezes')
 for c in range (1, 6):
     print('Oi!', c)
 print('Fim')
 
 # imprimindo uma mensagem 6 vezes de trás para frente
+print('\n')
+print('imprimindo uma mensagem 6 vezes de trás para frente')
 for c in range (6, 0, -1):
     print('Oi!', c)
 print('Fim')
