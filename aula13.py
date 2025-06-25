a482970a9c205833dbbda8f26b1a0636a6317e56
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 15:20:02 -0300

    Update aula13

--- aula13.py
+++ aula13.py
@@ -1,3 +1,10 @@
+# imprimindo uma mensagem 6 vezes
 for c in range (0, 6):
-    print('Oi!')
+    print('Oi!', c)
 print('Fim')
+
+# imprimindo uma mensagem 5 vezes
+for c in range (1, 6):
+    print('Oi!', c)
+print('Fim')
+
