b199fdb95cbc38faed4b760583256f8f24d1acc7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 19:19:51 -0300

    Update ex046

--- ex046.py
+++ ex046.py
@@ -1,11 +1,15 @@
 from time import sleep
+import random
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 sleep(1)
 for c in range(10, -1, -1):
-    print(c)
+    cor = random.randint(31, 36)
+    print(' ' * 21, '\033[1;' + cor + 'm' c)
+    print('\033[m', end='\r')
     sleep(1)
-print('\033[1;33mBUUMM!!! \033[1;32mPOW!!! \033[1;31mBANG!!!')
+print('\n')
+print(' ' * 10, '\033[1;33mBUUMM!!! \033[1;32mPOW!!! \033[1;31mBANG!!!')
 print('\033[m')
 print('\n' * 3)
