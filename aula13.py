b4930b7699b588c67c80d0051fe10612cf493d5d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 15:45:01 -0300

    Update aula13

--- aula13.py
+++ aula13.py
@@ -25,3 +25,14 @@ print('imprimindo uma mensagem pulando de 2 em 2')
 for c in range (0, 7, 2):
     print('Oi!', c)
 print('Fim')
+
+# imprimindo uma mensagem com números digitados pelo usuário
+print('\n')
+print('imprimindo uma mensagem com números digitados pelo usuário')
+i = int(input('Digite o início: '))
+f = int(input('Digite o fim: '))
+p = int(input('Digite o passo: '))
+for c in range(i, f+1 , p):
+    print(c)
+
+print('\n' * 3)
