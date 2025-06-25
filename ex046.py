e55771f98d84a2421fe5c008cf6870da4ebc94d0
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-25 19:25:13 -0300

    Update ex046

--- ex046.py
+++ ex046.py
@@ -5,8 +5,9 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 sleep(1)
 for c in range(10, -1, -1):
-    cor = random.randint(31, 36)
-    print(' ' * 21, '\033[1;' + cor + 'm' c)
+    cor = random.choice(['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m'])
+    print(cor)
+    print(' ' * 21, c)
     print('\033[m', end='\r')
     sleep(1)
 print('\n')
