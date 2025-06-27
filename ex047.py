461433f0298b78ac27fa6e9f030d86a79b3072ab
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-27 14:35:37 -0300

    Update ex047

--- ex047.py
+++ ex047.py
@@ -1,1 +1,13 @@
-# ex047
+# imprimindo numeros pares de 0 a 50
+import os
+import time
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print('Imprimindo números pares de 0 a 50')
+
+for c in range(2, 51, 2):
+    time.sleep(0.3)  # Atraso de 0.1 segundos entre os números
+    print(c, end=' ')
+print('\nFim da contagem de números pares de 0 a 50')
+print('\n' * 3)
