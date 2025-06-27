7b721f3783accdba1bdf89e296d94d5f2d16774a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-27 19:07:30 -0300

    Update ex048

--- ex048.py
+++ ex048.py
@@ -9,11 +9,10 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print('Soma dos números ímpares múltiplos de 3 no intervalo de 1 a 500')
 print('\n')
 
-for c in range(1, 501):
-    if c % 2 != 0:  # Verifica se o número é ímpar
-        if c % 3 == 0:
-            s += c  # Soma os números ímpares múltiplos de 3
-            tn += 1
+for c in range(1, 501, 2):  # Percorre os números de 1 a 500, pulando de 2 em 2 (ou seja, só os ímpares)
+    if c % 3 == 0:
+        s += c  # Soma os números ímpares múltiplos de 3
+        tn += 1
 
 print(f'A soma de todos os {tn} valores solicidados é: {s}')
 print('\n')
