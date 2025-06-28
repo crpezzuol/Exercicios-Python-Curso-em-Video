238df976dc344ce4f38f283024829b0a542b2129
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-27 21:43:23 -0300

    Update ex049

--- ex049.py
+++ ex049.py
@@ -13,4 +13,5 @@ print('\n\033[m')
 for c in range(1, 11):
     print(f'{num} x {c:2} = {num * c:3}')
     sleep(0.3)  # Atraso de 0.3 segundos entre os números
+print('\033[1;31m=' * 20, '\033[34mFIM DA TABUADA', '\033[1;31m=' * 20)  
 print('\n' * 3)
