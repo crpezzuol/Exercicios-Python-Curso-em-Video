5b48ec563c437df95db5037ef1772719f416232d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-27 21:48:54 -0300

    Update ex049

--- ex049.py
+++ ex049.py
@@ -10,10 +10,12 @@ print('\n\033[m')
 
 num = int(input('Digite um número para ver sua tabuada: \033[1;32m'))
 print('\n\033[m')
+sleep(0.5)  # Atraso de 0.5 segundos antes de iniciar a tabuada
 
 for c in range(1, 11):
     print(f'{num} x {c:2} = {num * c:3}')
     sleep(0.3)  # Atraso de 0.3 segundos entre os números
+
 print('\n') 
 print('\033[1;31m=' * 20, '\033[34mFIM DA TABUADA', '\033[1;31m=' * 21)
 print('\n' * 3, '\033[m')
