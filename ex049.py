65777422631ffd573f732b2e44b9501992299f66
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-27 21:28:10 -0300

    Update ex049

--- ex049.py
+++ ex049.py
@@ -1,6 +1,8 @@
 # ex049
 print('\n' * 3)
-print('=' * 20, 'TABUADA', '=' * 20)
+print('\033[1;31]=' * 20, 'TABUADA', '=' * 20)
 print('\n')
 num = int(input('Digite um número para ver sua tabuada: '))
-
+print('\n')
+for c in range(1, 11):
+    print(f'{num} x {c:2} = {num * c:3}')
