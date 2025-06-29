31c243a4bb26fb24803e29fc829a6729ce69aa40
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:41:15 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -6,6 +6,9 @@ soma = 0  # s = soma dos números pares solicitados
 os.system('cls' if os.name == 'nt' else 'clear')
 print('\n' * 3)
 
+print('\033[1;31m=' * 24, '\033[34mSOMA DOS NÚMEROS PARES', '\033[1;31m=' * 24)
+print('\n\033[m')
+
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: '))
     for i in range(2, num+1, 2):
