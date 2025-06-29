161a083cded84b445da841f8a15ddc0dee32f93e
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:27:05 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -7,5 +7,5 @@ for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: '))
     for i in range(2, num+1, 2):
         if num == 1:
-            
-            break
+            soma += num
+        
