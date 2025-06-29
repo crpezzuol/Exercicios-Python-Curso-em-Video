c7b45b300574e0d3a01a2975a7cfa182869953ef
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:42:39 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -11,8 +11,8 @@ print('\n\033[m')
 
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: '))
+    
     for i in range(2, num+1, 2):
-        print (f'Número par: {i}')
         if num == i:
             soma += num
 print(f'A soma dos números pares digitados é: {soma}')
