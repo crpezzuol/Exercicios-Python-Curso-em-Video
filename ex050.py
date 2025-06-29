c8392ed65ebb10e4ec613f9faa8ed88e3d5a05d8
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:58:19 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -12,6 +12,10 @@ print('\n\033[m')
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: \033[1;32m'))
     print('\n\033[m')
+    if num > 9999999:
+        print('\n\033[1;31mNúmero inválido! \033[mDigite um número inteiro menor que \033[1;31m10.000.000\033[m\n')
+        num = int(input(f'Digite o {c}º número inteiro: \033[1;32m'))
+        print('\n\033[m')
 
      # Verifica se o número é par
     for i in range(2, num+1, 2):
