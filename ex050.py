ea45b89df757a03510f85eabc68d807f7929e1fd
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:33:16 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -8,6 +8,11 @@ print('\n' * 3)
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: '))
     for i in range(2, num+1, 2):
-        if num == 1:
-            soma += num
-            print(soma)
+        
+
+
+
+
+#        if num == 1:
+ #           soma += num
+  #          print(soma)
