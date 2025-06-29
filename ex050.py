058e39e05fe45fe2e2f80d61a8eeaaf729347ad7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 19:55:43 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -4,5 +4,5 @@ from time import sleep
 os.system('cls' if os.name == 'nt' else 'clear')
 print('\n' * 3)
 for c in range(1, 7):
-    soma = float(input(f'Digite o {c}º número: '))
-    
+    num = float(input(f'Digite o {c}º número: '))
+    if num
