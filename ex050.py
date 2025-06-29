87732e9e388503eab3dceb8c3a53e9d08eed949f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 20:25:29 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -5,3 +5,7 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print('\n' * 3)
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: '))
+    for i in range(2, num+1, 2):
+        if num == 1:
+            
+            break
