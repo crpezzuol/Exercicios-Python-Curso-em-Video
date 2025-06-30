f3ccd1f40cfff0cef89b467a0f31b94695618976
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 21:30:47 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -2,3 +2,10 @@ import os
 from time import sleep
 
 os.system('cls' if os.name == 'nt' else 'clear')
+
+soma = 0  # s = soma dos números pares
+
+for c in range(1, 7):
+    num = int(input(f'Digite o {c}º número: '))
+    if num % 2 == 0:
+        soma += num
