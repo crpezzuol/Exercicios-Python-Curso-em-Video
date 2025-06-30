2736f8d33c4e1ae6a57f195b050faa5a38044645
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-29 21:02:51 -0300

    Update ex050

--- ex050.py
+++ ex050.py
@@ -4,14 +4,13 @@ from time import sleep
 soma = 0  # s = soma dos números pares solicitados
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print('\n' * 3)
 
 print('\033[1;31m=' * 24, '\033[34mSOMA DOS NÚMEROS PARES', '\033[1;31m=' * 24)
 print('\n\033[m')
 
 for c in range(1, 7):
     num = int(input(f'Digite o {c}º número inteiro: \033[1;32m'))
-    print('\n\033[m')
+    print('\033[m')
 
     if num > 9999999:
         print('\n\033[1;31mNúmero inválido! \033[mDigite um número inteiro menor que \033[1;31m10.000.000\033[m\n')
