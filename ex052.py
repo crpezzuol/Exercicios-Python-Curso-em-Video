aafc62db4fac9ebbea6c6d623468e0183030395a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-01 17:17:44 -0300

    Update ex052

--- ex052.py
+++ ex052.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 
 os.system('cls' if os.name == 'nt' else 'clear')
+cont = 0
 
 print ('\033[1;32m=' * 66)
 print('\033[1;33m>' * 25 ,'\033[1;34mNÚMEROS PRIMOS', '\033[1;33m<' * 25)
@@ -10,11 +11,13 @@ print('\033[m')
 num = int(input('Digite um número: \033[1;34m'))
 print('\033[m')
 sleep(0.2)
-print('\033[1;34mAnalisando...')
+print('\033[1;32mAnalisando...')
 print('\033[m')
 sleep(0.5)
 for c in range(1, num + 1):
     if num % c == 0:
         print(f'\033[1;32m{c}\033[m', end='\033[1;35m ► \033[m')  # alt + 16
+        cont += 1
     else:
         print(f'\033[1;31m{c}\033[m', end='\033[1;35m ► \033[m')  # alt + 16
+print('\033[1;33mFIM\033[m\n')
