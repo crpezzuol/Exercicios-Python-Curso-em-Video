3496a22d0e4787c50c414b1d0205b10055b08407
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-01 16:30:55 -0300

    Update ex052

--- ex052.py
+++ ex052.py
@@ -5,3 +5,5 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 print ('\033[1;32m=' * 66)
 print('\033[1;33m>' * 25 ,'\033[1;34mNÚMEROS PRIMOS', '\033[1;33m<' * 25)
+print ('\033[1;32m=' * 66)
+print('\033[m')
