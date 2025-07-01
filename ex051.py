250d3edfc92763ff081db06753fd273134878d3d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-30 23:26:30 -0300

    Update ex051

--- ex051.py
+++ ex051.py
@@ -5,7 +5,7 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 print ('\033[1;32m=' * 71)
 print('\033[1;33m>' * 25 ,'\033[1;34m10 TERMOS DE UMA PA', '\033[1;33m<' * 25)
-print ('\033[1;32m=' * 41)
+print ('\033[1;32m=' * 71)
 print('\033[m')
 primeiro = int(input('Primeiro termo: \033[1;32m'))
 print('\033[m')
