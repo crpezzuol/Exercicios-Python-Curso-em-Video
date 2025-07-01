e4aeeeb17471b675e712f382ff8de3614001f619
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-30 22:42:21 -0300

    Update ex051

--- ex051.py
+++ ex051.py
@@ -3,6 +3,6 @@ from time import sleep
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print ('=' * 41)
+print ('\033[1;32m=' * 41)
 print('>' * 10 ,'10 TERMOS DE UMA PA', '<' * 10)
 print ('=' * 41)
