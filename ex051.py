5bab8680d5e7e74fa44d3a0ad9d91e2015b99f73
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-06-30 22:45:16 -0300

    Update ex051

--- ex051.py
+++ ex051.py
@@ -5,4 +5,5 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 print ('\033[1;32m=' * 41)
 print('\033[1;33m>' * 10 ,'\033[1;34m10 TERMOS DE UMA PA', '\033[1;33m<' * 10)
-print ('=' * 41)
+print ('\033[1;32m=' * 41)
+print('\033[m')
