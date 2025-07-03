073c9ea95cea101cf9982d690fe3a292a957bc20
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-03 17:22:00 -0300

    Update ex053

--- ex053.py
+++ ex053.py
@@ -3,4 +3,3 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 nome = str(input('Digite seu nome completo: ')).strip().upper()
 if 'SILVA' in nome:
-    print('O nome contém "SILVA".')
