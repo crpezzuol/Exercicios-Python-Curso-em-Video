0bc2b7c9cd3e93d3477d494723916aae51cb4cf6
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-03 18:31:27 -0300

    Update ex053
    Exemplo de código para verificar se uma frase é um palíndromo

--- ex053.py
+++ ex053.py
@@ -1,1 +1,13 @@
-# ex053
+# Exemplo de código para verificar se uma frase é um palíndromo
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+frase = input('Digite uma frase: ').replace(' ', '').upper()
+print(f'O inverso de {frase} é {frase[::-1].upper()}')
+# Verifica se a frase é um palíndromo
+# A frase é um palíndromo se for igual à sua versão invertida
+if frase == frase[::-1]:
+    print('A frase é um palíndromo!')
+else:
+    print('A frase não é um palíndromo.')
