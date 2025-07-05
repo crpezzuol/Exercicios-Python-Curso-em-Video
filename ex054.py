a08c585b7d7a1729baa24a27570761249b836553
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 22:58:00 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -8,7 +8,8 @@ maior = 0   # Contador de maiores de idade
 menor = 0   # Contador de menores de idade
 idade = 0  # Variável para armazenar a idade calculada
 
-
+print('\033[1;34m-=' * 20)
+print('Verificador de Maioridade')
 
 # Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
