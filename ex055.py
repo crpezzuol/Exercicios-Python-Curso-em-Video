7c2b0d1de03e7b945783fc7cdd5ad82592a66ce6
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-05 20:06:19 -0300

    Update ex055

--- ex055.py
+++ ex055.py
@@ -3,6 +3,8 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+
+# Exercicio 055 - Maior e Menor Peso
 for c in range(1, 8):
     peso = float(input(f'Peso da {c}ª pessoa: '))
     if peso > maior:
