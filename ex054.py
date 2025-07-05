4d757284a0bb770f097fa52d49c45921c9c396e5
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 21:50:29 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -1,7 +1,10 @@
 
 import os
+import datetime
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+# Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
     idade = int(input(f'Digite a idade da {c}ª pessoa: '))
+    
