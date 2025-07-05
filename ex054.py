893925b9078fac21870162da1560782d472288c5
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 21:58:16 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -7,5 +7,10 @@ hoje = datetime.date.today().year
 
 # Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
-    idade = int(input(f'Digite a idade da {c}ª pessoa: '))
-    
+    nascido = int(input(f'Digite a idade da {c}ª pessoa: '))
+    idade = hoje - nascido
+    if nascido >= 18:
+        maior += 1
+    else:
+        menor += 1
+print(f'Temos {maior} pessoas maiores de idade e {menor} pessoas menores de idade.')
