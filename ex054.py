8065e2d940eaeaf8dcd531e6ad0b0d2b1e85cfdc
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 22:06:52 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -6,12 +6,15 @@ os.system('cls' if os.name == 'nt' else 'clear')
 hoje = datetime.date.today().year
 maior = 0   # Contador de maiores de idade
 menor = 0   # Contador de menores de idade
+idade = 0  # Variável para armazenar a idade calculada
 
 # Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
     nascido = int(input(f'Digite a idade da {c}ª pessoa: '))
-    # Calcula a idade com base no ano atual
     idade = hoje - nascido
+    print(f'nascido: {nascido} hoje: {hoje} idade: {idade}')
+    # Calcula a idade com base no ano atual
+   
     if nascido < 18:
         menor += 1
     else:
