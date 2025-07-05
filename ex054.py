5c7e512b289bc316ae11c81b11675debeaa3b4b7
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 21:51:24 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -3,6 +3,7 @@ import os
 import datetime
 
 os.system('cls' if os.name == 'nt' else 'clear')
+hoje = datetime.date.today().year
 
 # Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
