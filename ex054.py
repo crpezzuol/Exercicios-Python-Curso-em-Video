4ba8381cd8177333b74419d5e1aefa8c264c5a65
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-04 21:58:55 -0300

    Update ex054

--- ex054.py
+++ ex054.py
@@ -1,9 +1,10 @@
-
+# Exemplo de código para calcular a quantidade de maiores e menores de idade
 import os
 import datetime
 
 os.system('cls' if os.name == 'nt' else 'clear')
 hoje = datetime.date.today().year
+
 
 # Solicita a idade de 7 pessoas e calcula a quantidade de maiores e menores de idade
 for c in range (1, 8):
