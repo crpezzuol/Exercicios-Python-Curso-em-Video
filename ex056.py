861f680b326aeabc515f0bf173da168388573e88
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-07 18:45:56 -0300

    Update ex056

--- ex056.py
+++ ex056.py
@@ -6,6 +6,6 @@ os.system('cls' if os.name == 'nt' else 'clear')
 for c in range(1, 5):
     print(f'{"\033[1;34m>" * 10} \033[1;35m{c}ª PESSOA {"\033[1;34m<" * 10}')
     print('\033[m')
-    nome = str(input(f'Digite seu NOME: '))
+    nome = str(input(f'Digite seu NOME: \033[1;32m')).strip().capitalize()
     idade = int(input(f'Digite a idade de {nome}: '))
     sexo = str(input(f'Digite o sexo de {nome} (M/F): ')).strip().upper()
