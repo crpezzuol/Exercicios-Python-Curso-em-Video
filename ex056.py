f5f4280b6907e437aacc962212108697ef44dbe8
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-07 18:35:12 -0300

    Update ex056

--- ex056.py
+++ ex056.py
@@ -5,3 +5,4 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 for c in range(1, 5):
     nome = str(input(f'Digite o nome da {c}ª pessoa: '))
+    idade = int(input(f'Digite a idade de {nome}: '))
