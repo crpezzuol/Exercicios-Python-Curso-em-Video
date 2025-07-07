a90332d92c433e7fb11e1982a63ed23b40b42cec
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-07 18:37:58 -0300

    Update ex056

--- ex056.py
+++ ex056.py
@@ -4,7 +4,7 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 for c in range(1, 5):
-    print(f'{"-" * 20} {c}ª PESSOA {"-" * 20}')
+    print(f'{"-" * 6} {c}ª PESSOA {"-" * 6}')
     nome = str(input(f'Digite o nome da {c}ª pessoa: '))
     idade = int(input(f'Digite a idade de {nome}: '))
     sexo = str(input(f'Digite o sexo de {nome} (M/F): ')).strip().upper()
