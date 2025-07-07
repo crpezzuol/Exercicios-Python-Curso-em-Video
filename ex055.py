2a3dce64d868b49c86307e86146b91a3f172f5d2
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-06 22:49:25 -0300

    Update ex055

--- ex055.py
+++ ex055.py
@@ -22,6 +22,6 @@ for c in range(1, 8):
             maior = peso
         elif peso < menor:
             menor = peso    
-print(f'O maior peso lido foi \033[1;31m{maior}\033[mkg.')
+print(f'O maior peso lido foi \033[1;31m{maior}\033[m kg.')
 print(f'O menor peso lido foi {menor}kg')
 print('\n')
