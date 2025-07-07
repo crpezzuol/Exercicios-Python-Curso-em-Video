45836bebede76bd0af46c9009a175b1c2a8c8277
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-06 22:56:36 -0300

    Update ex055

--- ex055.py
+++ ex055.py
@@ -24,7 +24,8 @@ for c in range(1, 8):
             menor = peso    
 print(f'O maior peso lido foi \033[1;31m{maior}\033[m kg.')
 print(f'O menor peso lido foi \033[1;32m{menor}\033[m kg.')
-print('\n')
+
 print('\033[1;33m=' * 50)
+print('\n')
 
 # Fim do arquivo ex055.py
