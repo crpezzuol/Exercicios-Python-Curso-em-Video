eb8bdef9c65ad2c7854cf9bba11c6032f35f6dd6
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-07 22:40:20 -0300

    Update ex056

--- ex056.py
+++ ex056.py
@@ -24,7 +24,7 @@ for c in range(1, 5):
     elif sexo == 'M' and idade > maior_idade:
         maior_idade = idade
         nome_maior_idade = nome
-    if sexo == 'F' and idade < 20:
+    elif sexo == 'F' and idade < 20:
 
 
 print(f'Amédia de idade do grupo: \033[1;32m{soma_idade / 4:.1f}\033[m anos')
