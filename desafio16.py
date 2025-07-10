649c70049ef2a59b77627ce3976d15870901fb6f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-09 21:44:27 -0300

    numeros = random.sample(range(1, 81), 5)

--- desafio16.py
+++ desafio16.py
@@ -8,8 +8,11 @@ print('\033[1;33m>' * 38 + 'X' + '<' * 38)
 print('>' * 30, '\033[1;34mLOTERIA - QUINA', '\033[1;33m<' * 30)
 print('>' * 38 + 'X' + '<' * 38)
 print('\033[m')
+
 print('\033[1;32mSorteando 5 números entre 1 e 80...\033[m')
 numeros = random.sample(range(1, 81), 5)
 print('\033[1;33mOs números sorteados foram:\033[m', end=' ')
+for numero in numeros:
+    print(f'\033[1;35m{numero:02d}\033[m', end=' ')
 
-print('\033[m')
+print('\n\033[m')
