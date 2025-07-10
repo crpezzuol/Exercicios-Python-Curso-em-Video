b0922fd815e436fcf84c7a1f329db20532ab490d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-09 21:40:59 -0300

    Update desafio16

--- desafio16.py
+++ desafio16.py
@@ -8,6 +8,8 @@ print('\033[1;33m>' * 38 + 'X' + '<' * 38)
 print('>' * 30, '\033[1;34mLOTERIA - QUINA', '\033[1;33m<' * 30)
 print('>' * 38 + 'X' + '<' * 38)
 print('\033[m')
+print('\033[1;32mSorteando 5 números entre 1 e 80...\033[m')
+numeros = random.sample(range(1, 81), 5)
 
 
 print('\033[m')
