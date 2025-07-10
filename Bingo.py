59b6b13f7cea7e4583a70e5940cadb8526e630d5
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-10 15:45:00 -0300

    Update bingo

--- Bingo.py
+++ Bingo.py
@@ -59,7 +59,7 @@ def main():
     # Por exemplo, gerar números aleatórios, verificar vencedores, etc.
     print('\n\033[1;31mO jogo ainda não está implementado!')
     print('\n' * 2 + "\033[1;32mObrigado por jogar!")
-    print("Volte sempre!")
+    print('\n' * 2 + "Volte sempre!")
     print('-=' * 46)
     print("\n" * 2)
     exit()
