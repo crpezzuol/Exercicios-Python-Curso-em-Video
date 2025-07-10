5a08fb05bb200b5454234c0fed4f99837c946687
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-09 22:09:22 -0300

    Update desafio16

--- desafio16.py
+++ desafio16.py
@@ -10,7 +10,7 @@ print('>' * 30, '\033[1;34mLOTERIA - QUINA', '\033[1;33m<' * 30)
 print('>' * 38 + 'X' + '<' * 38)
 print('\033[m')
 print('\033[1;33m>' * 23, '\033[1;34mESCOLHA 5 NÚMEROS ENTRE 1 E 80', '\033[1;33m<' * 22)
-print('\n')
+
 print('\n\033[1;31mAtenção! \033[1;33mVocê só pode escolher números entre 1 e 80.\033[m')
 print ('\n')
 print('\033[1;32mSORTEANDO 5 NÚMEROS, AGUARDE...\n')
