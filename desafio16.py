d2bb53ca7090a670748303fd67458c96fae3250b
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-09 21:35:33 -0300

    Update desafio16

--- desafio16.py
+++ desafio16.py
@@ -4,7 +4,7 @@ import random
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print('\033[1;33m>' * 38 + '\033[1;34mX' + '<' * 38)
+print('\033[1;33m>' * 38 + '\033[1;34mX' + '\033[1;33m<' * 38)
 print('>' * 30, 'LOTERIA - QUINA', '<' * 30)
 print('>' * 38 + 'X' + '<' * 38)
 
