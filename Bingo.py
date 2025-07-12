99b8588cfd935e65a77659f595062e79b4cdc57b
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-11 22:29:29 -0300

    Update bingo

--- Bingo.py
+++ Bingo.py
@@ -14,7 +14,7 @@ def regras():
     cabecalho()  # Chama a função cabeçalho para exibir o cabeçalho do jogo
     print("\033[1;33m>" * 36, "\033[1;32mRegras  Jogo Bingo", "\033[1;33m<" * 36)
     print("\n\033[m")
-    print("1. O jogo é jogado com um cartão de Bingo que contém números.")
+    print("1. O jogo é jogado com um cartão de Bingo que contém 15 números.")
     print("2. Um número é sorteado aleatoriamente e anunciado.")
     print("3. Os jogadores marcam o número em seus cartões se estiver presente.")
     print("4. O primeiro jogador a completar uma linha, coluna ou diagonal ganha.")
