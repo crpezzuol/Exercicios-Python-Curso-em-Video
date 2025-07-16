293f69e5504bacd6f1a35d2d9dc74d1d4e204cee
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-15 22:10:31 -0300

    Update ex057

--- ex057.py
+++ ex057.py
@@ -1,6 +1,5 @@
  # ex057
 
-m = f = ''
 while m != 'M' or f != 'F':
     s = str(input('Informe seu sexo [M/F]: ')).strip().upper()    
     ('Dados inválidos. Por favor, informe seu sexo [M/F]: ').strip().upper()
