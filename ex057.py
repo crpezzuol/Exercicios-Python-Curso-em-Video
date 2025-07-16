077cb18ce2a6c802fa2104a773e492c8968d10aa
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-15 22:22:10 -0300

    Update ex057

--- ex057.py
+++ ex057.py
@@ -1,7 +1,7 @@
  # ex057
 
 s = str(input('Informe seu sexo [M/F]: ')).strip().upper()
-while s != 'M' or s != 'F':    
+while s != 'M':    
     s = str(input('Dados inválidos. Por favor, informe seu sexo [M/F]: ')).strip().upper()
 if s == 'M':
     print('Sexo "MASCULINO" registrado com sucesso!')
