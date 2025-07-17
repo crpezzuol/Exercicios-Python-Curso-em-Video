ec359158949cc61d701aafc2b2d08fa6b30e77aa
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-17 15:54:57 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -1,7 +1,8 @@
  # ex058
 from time import sleep
+import random
 
 print('Sou seu computador...')
 sleep(0.5)
 print('Acabei de pensar em um número entre 0 e 10.')
-
+print('Será que você consegue adivinhar qual foi?')
