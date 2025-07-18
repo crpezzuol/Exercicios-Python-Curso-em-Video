7b1db22b14c216d1a32c813cd552c2d4126653d9
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 01:05:55 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -36,7 +36,7 @@ else:
 
 
 # solução curso em video
-
+'''
 computador = randint(0, 10)
 print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue adivinhar qual foi?')
@@ -53,3 +53,4 @@ while not acertou:
         elif jogador > computador:
             print('Menos... Tente mais uma vez')
 print(f'Acertou com {palpites} tentativas. Parabéns!')
+'''
