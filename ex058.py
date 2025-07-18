7527b31aee4b10b2bc69508b8b0906cc4d6ad684
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 00:54:43 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -34,6 +34,9 @@ else:
 
 
 # solução curso em video
+
+
+
 computador = randint(0, 10)
 print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue adivinhar qual foi?')
