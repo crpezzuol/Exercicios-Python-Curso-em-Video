fea561eaf0f6cd29f3d2e233b7f608ba91cf065e
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 01:21:25 -0300

    Update ex059

--- ex059.py
+++ ex059.py
@@ -2,4 +2,5 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-valor_1 = int(input('Digite o Primeiro valor: \033[1;32m'))
+valor_1 = int(input('Digite o primeiro valor: \033[1;32m'))
+valor_2 = int(input('\033[mDigite o segundo valor: \033[1;32m'))
