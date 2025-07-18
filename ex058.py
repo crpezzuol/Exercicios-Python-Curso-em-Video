d6444fdf008818a6dc99c879284bb19239bde27f
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 00:43:16 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -39,3 +39,4 @@ print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue adivinhar qual foi?')
 acertou = False
 palpites = 0
+if jogador = int(input('Qual é seu palpite? '))
