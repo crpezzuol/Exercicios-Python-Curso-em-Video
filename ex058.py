8c254917b4b64b52bb816409f77bf9ee28837e74
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-17 23:57:46 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -10,7 +10,7 @@ sleep(0.5)
 print('Estou pensando em um número entre 0 e 10...')
 sleep(1)
 print('Será que você consegue adivinhar qual foi?')
-palpite = int(input('Qual é o seu palpite?: \033[1;32m'))
+palpite = int(input('Qual é o seu palpite? : \033[1;32m'))
 print('\033[m')
 numero = random.randint(0, 10)
 tentativas += 1
