80e028dd56b50c68743016843225dcc4c20bf871
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 00:42:18 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -37,3 +37,5 @@ else:
 computador = randint(0, 10)
 print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
