cfa72e5d1a6c1d989d94dd7d5a36116b6fbd46ab
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 01:36:51 -0300

    Update ex059

--- ex059.py
+++ ex059.py
@@ -6,3 +6,10 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 valor_1 = int(input('Digite o primeiro valor: \033[1;32m'))
 valor_2 = int(input('\033[mDigite o segundo valor: \033[1;32m'))
+print('\033[m=-=' * 11)
+print('    [ 1 ] Somar' 
+'\n    [ 2 ] Multiplicar' 
+'\n    [ 3 ] Maior' \
+'\n    [ 4 ]  Novos números' 
+'\n    [ 5 ] Sair do Programa\n')
+opcao = int(input('>>>>> Qual é a sua opção? \033[1;32m'))
