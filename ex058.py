319ff09494e1da0432760787e665c8f4343a4e42
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-17 23:35:44 -0300

    Update ex058

--- ex058.py
+++ ex058.py
@@ -19,5 +19,5 @@ while palpite != numero:
         palpite += 1
     if palpite > numero:
         print('Menos... Tente mais uma vez!')
-
+        palpite = int(input('Qual é o seu palpite?: \033[1;32m'))
 
