f96188be2a0cdd763ebc2a8743f9275c6c58c38d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-18 21:11:09 -0300

    Updat ex059

--- ex059.py
+++ ex059.py
@@ -9,7 +9,7 @@ valor_2 = int(input('\033[mDigite o segundo valor: \033[1;32m'))
 print('\033[m=-=' * 11)
 print('    [ 1 ] Somar' 
 '\n    [ 2 ] Multiplicar' 
-'\n    [ 3 ] Maior' \
+'\n    [ 3 ] Maior' 
 '\n    [ 4 ] Novos números' 
 '\n    [ 5 ] Sair do Programa\n')
 opcao = int(input('>>>>> Qual é a sua opção? \033[1;32m'))
@@ -18,3 +18,7 @@ while opcao == 1:
     print(f'A soma entre {valor_1} + {valor_2} é {valor_1 + valor_2}')
     break
 print('=-=' * 11)
+    while opcao == 2:
+        print(f'A multiplicação entre {valor_1} * {valor_2} é {valor_1 * valor_2}')
+        
+print('\n')
