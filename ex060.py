cf686df5ce8c05bf4dde274d93345c6c02f0cbbe
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-19 00:23:07 -0300

    Update ex060

--- ex060.py
+++ ex060.py
@@ -6,7 +6,8 @@ import os
 os.system('cls' if os.name == 'nt' else ' clear')
 c = 0
 n = int(input('Digite um número para calcular seu fatorial: \033[1;32m'))
-while c == n:
+while c != n:
+   
     for c in range(1, n):
         print(f'\033[mCalculando {n}! =', end = ' x ')
         c += 1
