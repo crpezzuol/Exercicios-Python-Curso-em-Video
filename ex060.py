beb0f0eabfd848b2ffcd754c139a46eedbeefb4d
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-20 00:01:01 -0300

    Update ex060

--- ex060.py
+++ ex060.py
@@ -21,7 +21,7 @@ print('\n')
 os.system('cls' if os.name == 'nt' else 'clear')
 
 n = int(input('Digite um número para calcular seu fatorial: '))
-
+print('Calculando...')
 
 # 1 solução curso em video
 
