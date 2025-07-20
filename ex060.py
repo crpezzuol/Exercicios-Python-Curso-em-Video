5a6dbe36c3bb647a7f1027d4ab68cf783c982f84
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-19 23:36:30 -0300

    Update ex060

--- ex060.py
+++ ex060.py
@@ -8,6 +8,7 @@ os.system('cls' if os.name == 'nt' else ' clear')
 f = 1
 n = int(input('Digite um número para calcular seu fatorial: \033[1;32m'))
 print(f'\033[mCalculando {n}! =', end = '') 
+
 for c in range(n, 0, -1):
     f *= c
     print(f' {c} ', end = 'x')
