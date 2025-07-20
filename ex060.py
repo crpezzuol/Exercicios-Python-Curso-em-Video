5ac2d4edd4c8c28c906debfb5600cb79631cb615
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-07-19 23:55:04 -0300

    Update ex060

--- ex060.py
+++ ex060.py
@@ -21,4 +21,4 @@ print('\n')
 from math import factorial
 n = int(input('Digite um número para cacular seu fatorial: '))
 f = factorial(n)
-print('O fatorial de {n} é {f}')
+print(f'O fatorial de {n} é {f}')
